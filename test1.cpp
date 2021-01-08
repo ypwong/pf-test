5223dff2ccd752e18b9ec11d9703de1a9134657b
ypwong <ypwong@mmu.edu.my>
2021-01-09 01:03:14 +0800

    first commit

---
+++ test1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num;
+
+    cout << "Test 1";
+} 
---
+++ test2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num;
+
+    cout << "Enter a real number => ";
+    cin >> num;
+
+    cout << "You entered the number " << num << endl;
+} 
